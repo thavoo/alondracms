7df23d2255ea2b834984b34de41b840be9ebf41f
xD <cocoaremix@gmail.com>
2018-02-20 06:37:25 -0800

    updating all data

--- alondra/src/user/urls.py
+++ alondra/src/user/urls.py
@@ -1,5 +1,7 @@
 from user import rest_api as views 
 from django.conf.urls import include, url
+from rest_framework.authtoken import views as rest_framework_views
+
 register_url = True
 urlpatterns = [
     url(r'^login/$', views.login),
@@ -11,4 +13,5 @@ urlpatterns = [
     url(r'^new/user/$', views.user_create),
   	url(r'^me/$', views.user),
   	url(r'^me/password/$', views.user_udpate_password),	
+  	url(r'^get_auth_token/$', rest_framework_views.obtain_auth_token, name='get_auth_token'),
 ]
