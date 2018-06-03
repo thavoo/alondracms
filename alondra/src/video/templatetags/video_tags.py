230b98fc6d7d6bbd4d7d99fcb272d6c527347feb
xD <cocoaremix@gmail.com>
2018-06-03 10:54:18 -0700

    fixing errors

---
+++ alondra/src/video/templatetags/video_tags.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from django.conf import settings
+from django import template
+from posts.models import PostItem
+from utilities.paginator import paginator
+
+register = template.Library()
+@register.assignment_tag(name='get_related_videos', takes_context=True)
+def get_related_videos(context,parent_id,limit=5):
+    videos = []
+    headers = {
+        "User-Agent": "Gamajuegos Browser",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept": "application/json; charset=utf-8",
+        "Accept-Language": "es-MX,es;q=0.8",
+        "Connection": "keep-alive",
+        'Content-type': 'application/json; charset=utf-8'
+    }
+
+    r = requests.post(
+        settings.GET_VIDEO_RELATED_LIST, 
+        data=json.dumps({'parent_id':parent_id,'limit':limit}), 
+        headers=headers
+    )
+    r.encoding = 'utf-8'
+    if r.status_code == 200:
+       videos = json.loads(r.text.encode('utf-8'))
+    return videos
